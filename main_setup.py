8526178b13584a726a23726479e973e4b6caf08b
Victoria <tovictoriadao@gmail.com>
2024-06-10 17:05:26 -0400

    Created a player class in a separate file. In this class, it defines the character's dimensions, position, and speed. There is also a method that holds its movements (WASD). In the main setup file, I added the player class in as char and created the redrawGameWindow method. Now the character can move!

--- main_setup.py
+++ main_setup.py
@@ -1,4 +1,6 @@
+#~~~ Imports ~~~
 import pygame
+import player
 
 #Initialize pygame program
 pygame.init()
@@ -14,16 +16,45 @@ win = pygame.display.set_mode((winWidth, winHeight))
 #Create name of the window
 pygame.display.set_caption("Our Game")
 
+#Initialize the player
+char = player.Player(50, 250, 24, 24, 10)
+
+
+#Setup Window Boundaries
+widthBoundary = winWidth - char.width - char.speed
+heightBoundary = winHeight - char.height - char.speed
+
+#~~~ Functions ~~~ 
+def redrawGameWindow():
+    #Load/Update Background
+    win.fill("white")
+
+    #Draw the player
+    hitbox = pygame.Rect(char.x, char.y, char.width, char.height)
+    pygame.draw.rect(win, "black", hitbox)
+
+    #Update any changes
+    pygame.display.flip()
+
 
 #~~~ Main Loop ~~~
 #Toggles the Running status of the game (on/off)
 run = True
 while run:
+    #Loading time for game
+    pygame.time.delay(100)
+
     #Checks for any player interaction while running (mouse clicks, keyboard, etc.)
     for event in pygame.event.get():
         #Handle scenario when the player closes the window (X)
         if event.type == pygame.QUIT:
             run = False
         
+    keys = pygame.key.get_pressed()
+    char.movement(keys, widthBoundary, heightBoundary)
+    
+    #Update the window
+    redrawGameWindow()
+
 #When the game is off, close the pygame program as well.
 pygame.quit()
---
+++ player.py
@@ -0,0 +1,46 @@
+#Creating the Player class
+import pygame
+
+class Player():
+
+    # ~~ Character Location + Dimensions ~~
+        #Character (x-coor, y-coord, width, height)
+        #NOTE: Left-Right starts from 0 to 500
+        #NOTE: Up-Down starts from 0 to 500 as well
+
+    def __init__(self, x, y, width, height, speed):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.speed = speed
+        self.left = False
+        self.right = False
+        
+
+    #Changes the x and y coordinates of the player. 
+    #Requires the current key input, the width boundary and the height boundary of the window
+    def movement(self, keys, widthBounds, heightBounds):
+        #Checks the key and moves the character correspondingly
+            if keys[pygame.K_a] and self.x > self.speed:
+                self.x -= self.speed
+                left = False
+                right = True
+                #current_set = 1
+            elif keys[pygame.K_d] and self.x < widthBounds:
+                self.x += self.speed
+                left = True
+                right = False
+                #current_set = 1
+            elif keys[pygame.K_w] and self.y > self.speed:
+                self.y -= self.speed
+                left = True
+                right = False
+                #current_set = 1
+            elif keys[pygame.K_s] and self.y < heightBounds:
+                self.y += self.speed
+                left = True
+                right = False
+                #current_set = 1
+            #else:
+                #current_set = 0

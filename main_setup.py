c3473c69f1a8977b650f0ec837b38fd41f7033be
Victoria <tovictoriadao@gmail.com>
2024-06-12 15:41:44 -0400

    Added the collision mechanics into the game by creating 3 lists: trashPile, collectPile, and trashHitboxes. After creating trash objects, I add them all to trashPile list and then parse through the list to add their hitboxes in a separate list: trashHitboxes. Then I created a method called collectTrash(...) which takes the player's hitbox and checks through the trashHitboxes list to see if it has been collided. When the player does touch the trash, it will be removed from the trashPile list and added to the collectPile, hiding the trash completely.

--- main_setup.py
+++ main_setup.py
@@ -17,38 +17,63 @@ win = pygame.display.set_mode((winWidth, winHeight))
 #Create name of the window
 pygame.display.set_caption("Our Game")
 
+#Global Variables
+char_x = 250 #winWidth * 0.5
+char_y = 250 #winHeight * 0.65
+char_w = 100
+char_h = 100
+char_s = 40
+trashPile = []
+trashHitboxes = []
+collectPile = []
+
 #Initialize the player
 #(x, y, width, height, speed)
-char = player.Player(50, 250, 100, 100, 40)
-
-#Global Variables
-char_x = char.x
-char_y = char.y
-char_w = char.width
-char_h = char.height
-char_s = char.speed
+char = player.Player(char_x, char_y ,char_w, char_h, char_s)
 
 #Setup Window Boundaries
 widthBoundary = winWidth - char_w - char_s
 heightBoundary = winHeight - char_h - char_s
 
-#Initialize trash sprites
-trash = trash.Trash(char_w, char_h, char_s, widthBoundary, heightBoundary)
+#Create a list of trash objects and add their hitboxes in trashHitboxes
+for i in range(5):
+    trashPile.append(trash.Trash(char_w, char_h, char_s, widthBoundary, heightBoundary))
+
+for i in trashPile:
+    trashHitboxes.append(i.hitbox)
 
 #~~~ Functions ~~~ 
+#Checks the collision between trash object and the player
+#When they collide, add the trash object to the collectPile, update the spawn location, and hide it
+def collectTrash(player_hitbox):
+    #Check if the hitboxes collide base on the trashHitboxes list/Return index of collided rectangle
+    if pygame.Rect.collidelist(player_hitbox, trashHitboxes) != -1:
+        #Get the index of the trash that has been hit
+        collectTrash = player_hitbox.collidelist(trashHitboxes)
+
+        #Remove the trash from the trashPile and add it to the collectPile
+        collectPile.append(trashPile.pop(collectTrash))
+        
+        #Remove the old trash hitbox from the trashHitboxes
+        trashHitboxes.pop(collectTrash)
+
+#Update the game window with new animations/movement
 def redrawGameWindow():
     #Load/Update Background
     win.fill("white")
 
-    #Draw trash
-    # trash.trashHitbox()
-    # pygame.draw.rect(win, "red", trash.hitbox)
+    #Load trash that exists in trashPile
+    for trash in trashPile:
+        pygame.draw.rect(win, "red", trash.hitbox)
 
-    #Draw the player
+    #Load the player/Update player's movement
     char.playerHitbox()
     pygame.draw.rect(win, "black", char.hitbox)
 
-    #Update any changes
+    #Check collision and update trash lists accordingly
+    collectTrash(char.hitbox)
+
+    #Update/Finalize all changes made
     pygame.display.flip()
 
 
--- trash.py
+++ trash.py
@@ -2,18 +2,9 @@ import pygame
 import random
 
 class Trash():
-    global trashPile
-    global collectPile
-    #global trashHitbox
-
     def __init__(self, playerWidth, playerHeight, playerSpeed, widthBound, heightBound):
         self.width = random.randint(20, playerWidth - 20)
         self.height =  random.randint(20, playerHeight - 20)
         self.x = random.randint(playerSpeed, widthBound)
         self.y = random.randint(playerSpeed, heightBound)
-        trashPile = []
-        collectPile = []
-
-    def trashHitbox (self):
         self.hitbox = pygame.Rect(self.x, self.y, self.width, self.height)
-        self.hidden = False
